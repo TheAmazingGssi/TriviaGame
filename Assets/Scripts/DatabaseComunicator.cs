34b98b71a6e466cf6afa740772db4512926f1b20
TheAmazingGssi <105499786+TheAmazingGssi@users.noreply.github.com>
2025-07-12 12:48:47 +0300

    Initial commit

---
+++ Assets/Scripts/DatabaseComunicator.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class DatabaseComunicator : MonoBehaviour
+{
+    public int queryID;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        StartCoroutine(GetText());
+    }
+
+    IEnumerator GetText()
+    {
+        UnityWebRequest www = UnityWebRequest.Get("https://localhost:7270/api/Trivia/" + queryID);
+        Debug.Log("waiting for response");
+        yield return www.SendWebRequest();
+        Debug.Log("got response");
+
+        if (www.result == UnityWebRequest.Result.ConnectionError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            // Show results as text
+            Debug.Log(www.downloadHandler.text);
+            //UpdateQuestionUI(www.downloadHandler.text);
+        }
+
+        www.
+    }
+}
